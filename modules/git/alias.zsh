39efca2015f49bbea7bf575b7584af57e257a2b8
Eyal Lotem <eyal.lotem@gmail.com>
2016-11-11 00:46:19 +0200

    gs is status

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -231,7 +231,7 @@ if ! zstyle -t ':prezto:module:git:alias' skip 'yes'; then
   alias gRb='git-hub-browse'
 
   # Stash (s)
-  alias gs='git stash'
+  alias gs='git status'
   alias gsa='git stash apply'
   alias gsx='git stash drop'
   alias gsX='git-stash-clear-interactive'
